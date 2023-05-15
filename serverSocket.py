6569b8861098ab5fc660aedee4be0adbb56a9e5c
fredericodossantos <frederico.dos.santos@gmail.com>
2023-05-15 13:31:02 -0300

    first commit

---
+++ serverSocket.py
@@ -0,0 +1,8 @@
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#bind the socket  to localhost on port 80
+serversocket.bind(('localhost',80))
+#became a server socket and listen a maximum of 10 connections
+serversocket.listen(10)
+
